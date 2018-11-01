2d60b78e21049a2fe698ec0dee94f2761956eb24
minhthien192 <44235990+minhthien192@users.noreply.github.com>
2018-11-01 14:11:32 +0700

    Add files via upload

---
+++ DeleteMovie.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BUS;
+using Entity;
+
+namespace GUI
+{
+    public partial class DeleteMovie : Form
+    {
+        ManageMovie parent;
+        MovieBUS mb = new MovieBUS();
+        private int movieID;
+
+        public DeleteMovie(ManageMovie parent)
+        {
+            InitializeComponent();
+            this.parent = parent;
+        }
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            Movie movie = mb.GetMovieByID(movieID);
+            movie.State = false;
+            mb.UpdateMovie(movie, movieID);
+            DisplaySuccessNotify("Delete movie successfully!");
+            parent.LoadData();
+            this.Close();
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void DeleteMoviee_Load(object sender, EventArgs e)
+        {
+            //Get all data of movie from ManageMovie form
+            int rowindex = parent.dgvMovies.CurrentCell.RowIndex;
+            string id = parent.dgvMovies.Rows[rowindex].Cells[0].Value.ToString();
+            movieID = Convert.ToInt32(id);
+            string title = parent.dgvMovies.Rows[rowindex].Cells[1].Value.ToString();
+            string state = parent.dgvMovies.Rows[rowindex].Cells[2].Value.ToString();
+            string genreID = parent.dgvMovies.Rows[rowindex].Cells[3].Value.ToString();
+
+            //Dislay data at Delete form
+            lblMovieID.Text = id;
+            lblName.Text = title;
+            if (state.Equals("True"))
+            {
+                lblState.Text = "On showing";
+            }
+            else
+            {
+                lblState.Text = "Finished";
+            }
+            lblGenre.Text = mb.GetGenreName(Convert.ToInt32(genreID));
+        }
+
+        public void DisplaySuccessNotify(string str)
+        {
+            SuccessNotify notify = new SuccessNotify();
+            notify.lblMessage.Text = str;
+            notify.ShowDialog();
+        }
+
+        public void DisplayFailNotify(string str)
+        {
+            FailNotify notify = new FailNotify();
+            notify.lblMessage.Text = str;
+            notify.ShowDialog();
+        }
+    }
+}

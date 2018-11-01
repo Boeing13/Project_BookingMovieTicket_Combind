8b06c78fcb5b6ab033bb5b475fd9d13c735efbb5
minhthien192 <44235990+minhthien192@users.noreply.github.com>
2018-11-01 23:03:03 +0700

    Change entity - Movie

--- Entity/Movie.cs
+++ Entity/Movie.cs
@@ -11,19 +11,21 @@ namespace Entity
         public int MovieID { get; set; }
         public string Title { get; set; }
         public bool State { get; set; }
-        public int genreID { get; set; }
-        public string description { get; set; }
+        public int GenreID { get; set; }
+        public string Description { get; set; }
 
         public Movie()
         {
 
         }
 
-        public Movie(int movieID, string title, bool state)
+        public Movie(int movieID, string title, bool state, int genreID, string description)
         {
             this.MovieID = movieID;
             this.Title = title;
             this.State = state;
+            this.GenreID = genreID;
+            this.Description = description;
         }
     }
 }
